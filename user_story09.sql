d7731ca933d83d6197bcc8f203e7c4fb30a6e2ef
Sandalcho7 <138883157+Sandalcho7@users.noreply.github.com>
2024-01-09 17:10:53 +0100

    Update user_story09.sql

--- user_story09.sql
+++ user_story09.sql
@@ -4,3 +4,4 @@ JOIN foyers_fiscaux ff ON ts.ville = UPPER(ff.ville)
 WHERE ff.date = "2018"
       AND ff.revenu_fiscal_moyen > 70000
       AND ts.date_transaction LIKE '2022%'
+      AND type_batiment = 'Appartement'
